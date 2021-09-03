8bbbd6749ecaa2edf301a50e8911466d6f67be6e
Muhodari Sage <sagemuho@gmail.com>
2021-09-03 10:54:09 +0200

    working on user profile component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,28 @@
 @section('content')
 <div class="container">
 
+<div class="row">
+
+    <div class="col-3 p-5">
+     <img src="https://dpi.gov.gy/wp-content/uploads/2019/06/pictures-for-profile-200x200-2.png" alt=""  height="120px" class="rounded-circle">
+    </div>
+
+    <div class="col-9 pt-5">
+ <div><h1>freecodeCamp</h1></div>
+
+<div class="d-flex">
+ <div class="pr-5"><strong>153</strong>posts</div>
+ <div class="pr-5"><strong>23k</strong>followers</div>
+ <div class="pr-5"><strong>212</strong>following</div>
+</div>
+
+<div class="pt-4 font-weight-bold">freeCodeCamp</div>
+<div>programing is the key to the new development programing is the key to the new development programing is the key to the new development  </div>
+<div> <a href="#">www.freecodecamp.com</a></div>
+</div>
+
+</div>
+
+
 </div>
 @endsection
