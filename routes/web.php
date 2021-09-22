73198b14d279a7d004a3277ba9fd7d8cf48d9429
Muhodari Sage <sagemuho@gmail.com>
2021-09-22 14:47:58 +0200

    new laout added

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+Route::get('/p','PostsController@create');
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
