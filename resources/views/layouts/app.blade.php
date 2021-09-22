05b05f57850040d898badec7527c002a5dc6fcd2
Muhodari Sage <sagemuho@gmail.com>
2021-09-22 10:16:11 +0200

    added username

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div><img src="/svg/freeCodeCamp.logo.svg" alt="" style="height:25px; border-right:1px solid #333" class="pr-3"></div>
-                    <div class="pl-3 pt-1">freeCodeComp</div>
+                    <div><img src="/svg/ig.logo.svg" alt="" style="height:25px; border-right:1px solid #333" class="pr-3"></div>
+                    <div class="pl-3 pt-1">Instagram</div>
 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -53,7 +53,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
