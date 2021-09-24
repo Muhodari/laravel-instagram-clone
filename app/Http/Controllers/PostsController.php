bd9d858a6870a6dc26611f7a2fe8808fed6f24a4
Muhodari Sage <sagemuho@gmail.com>
2021-09-24 09:50:59 +0200

    added dynamic user

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -25,6 +26,8 @@ public function store(){
     ]);
 
    $imagePath = request('image')->store('uploads','public');
+   $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200,1200);
+   $image->save();
 
     auth()->user()->posts()->create([
         'caption'=> $data['caption'],
@@ -34,4 +37,11 @@ public function store(){
   return redirect('/profile/'.auth()->user()->id);
 
 }
+
+
+public function show(\App\Post $post){
+return view('posts.show',compact('post'));
+}
+
+
 }
