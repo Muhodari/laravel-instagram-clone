9554e232c9d7e476b1d55008095fb22366947233
Muhodari Sage <sagemuho@gmail.com>
2021-09-24 10:28:51 +0200

    finished edit profile form

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -12,9 +12,11 @@
     <div class="col-9 pt-5">
   <div class="d-flex justify-content-between align-items-baseline">
      <h1>{{$user->username}}</h1>
-     <a href="/p/create">Add new post</a>
 
-    </div>
+      <a href="/p/create">Add new post</a>
+
+  </div>
+        <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
 
 <div class="d-flex">
  <div class="pr-5"><strong>{{$user->posts->count()}} </strong>posts</div>
