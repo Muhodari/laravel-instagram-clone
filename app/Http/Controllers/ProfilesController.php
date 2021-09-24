f6224df91544b5330d88056c73608f728099fc56
Muhodari Sage <sagemuho@gmail.com>
2021-09-24 10:48:57 +0200

    added profile policy

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -16,4 +16,17 @@ class ProfilesController extends Controller
     public function edit(\App\User $user){
      return view('profiles.edit',compact('user'));
     }
+
+    public function update(User $user){
+        $data = request()->validate([
+            'title'       =>'required',
+            'description' =>'required',
+            'url'         => 'url',
+            'image'       => ''
+        ]);
+       auth()->$user->profile()->update($data);
+
+    return redirect("/profile/{$user->id}");
+    }
+
 }
