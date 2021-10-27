f50ae7c65010255b390ae2b5a53f0021967cf856
Muhodari <sagemuho@gmail.com>
2021-10-27 21:35:49 +0200

    sized post layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,29 +3,22 @@
 @section('content')
 <div class="container">
 @foreach($posts as $post)
-        <div class="row">
-            <div class="col-8" >
 
-                <img src="/storage/{{$post->image}}" class="w-100">
+        <div class="row">
+            <div class="col-6 offset-3" >
+
+                <a href="/profile/{{$post->user->id}}">
+                    <img src="/storage/{{$post->image}}" class="w-100">
+                </a>
             </div>
 
-            <div class="col-4">
+        </div>
 
-                <div class="d-flex align-items-center">
+    <div  class="row pt-2 pb-4">
 
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" alt=""
-                             style="max-width:43px">
-                    </div>
+            <div class="col-6 offset-3">
 
-                    <div>
-                        <div class="font-weight-bold"><a href="/profile/{{$post->user->id}}">
-                                <span class="text-dark"> {{$post->user->username}}</span></a>
-                            <a href="#" class="pl-3">Follow</a>
-                        </div>
-                    </div>
 
-                </div>
 
                 <hr>
 
